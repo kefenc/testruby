49d07c7b5c5a599a183907e919c13c88c516642f
kefen <e0425865@u.nus.edu>
2021-05-24 03:28:09 +0800

    bootstrap table added

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,26 +1,40 @@
 class EmployeeController < ApplicationController
     require 'rest-client'
+    require 'excon'
+    require 'json'
+    require 'ostruct'
 
     def index
     end
   
     def search
       employees = find_employee(params[:employee])
+      @employee = employees
       unless employees
         flash[:alert] = 'Employee not found'
         return render action: :index
       end
-      @employee = employees.first
+      # ...
     end
   
     # app/controllers/travel_controller.rb
     private
   
+   
+
+    # def request_api(url)
+    #   response = RestClient.get(url)
+    #   print("response: ", response)
+    #   object = JSON.parse(response, object_class: OpenStruct)
+    #   print(object._embedded.employeeList)
+    #   JSON.parse(object.toString())
+    # end
+
     def request_api(url)
       begin
         response = RestClient.get(url)
-        render json: response
-        
+        print("response: ", response)
+        JSON.parse(response)
       rescue
          return nil
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'employee#index'
+
+  get '/search' => 'employee#search'
 end
